3950db2e7e08422e0576328e649a4dce7df6efe6
gregos <troch.gregory@gmail.com>
2018-01-30 17:53:44 +0100

    commit

--- src/FDM/ChampBundle/Controller/DefaultController.php
+++ src/FDM/ChampBundle/Controller/DefaultController.php
@@ -8,6 +8,12 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+    	$ch = curl_init ( "http://www.lfp.fr/ligue1/calendrier_resultat?sai=100#sai=2&jour=1" );
+    	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+    	$res = curl_exec ( $ch );
+    	curl_close ( $ch );
+    	echo htmlentities($res);
+//     	echo $res;
         return $this->render('FDMChampBundle:Default:index.html.twig');
     }
 }
---
+++ src/FDM/ChampBundle/Entity/AncienNomClub.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace FDM\ChampBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * AncienNomClub
+ *
+ * @ORM\Table(name="ancien_nom_club")
+ * @ORM\Entity(repositoryClass="FDM\ChampBundle\Repository\AncienNomClubRepository")
+ */
+class AncienNomClub
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ancienNomClub", type="string", length=255)
+     */
+    private $ancienNomClub;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateANC", type="datetime")
+     */
+    private $dateANC;
+    
+    /**
+     * 
+     * @ORM\ManyToOne(targetEntity="FDM\ChampBundle\Entity\Club")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $clubANC;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set ancienNomClub
+     *
+     * @param string $ancienNomClub
+     *
+     * @return AncienNomClub
+     */
+    public function setAncienNomClub($ancienNomClub)
+    {
+        $this->ancienNomClub = $ancienNomClub;
+
+        return $this;
+    }
+
+    /**
+     * Get ancienNomClub
+     *
+     * @return string
+     */
+    public function getAncienNomClub()
+    {
+        return $this->ancienNomClub;
+    }
+
+    /**
+     * Set dateANC
+     *
+     * @param \DateTime $dateANC
+     *
+     * @return AncienNomClub
+     */
+    public function setDateANC($dateANC)
+    {
+        $this->dateANC = $dateANC;
+
+        return $this;
+    }
+
+    /**
+     * Get dateANC
+     *
+     * @return \DateTime
+     */
+    public function getDateANC()
+    {
+        return $this->dateANC;
+    }
+
+    /**
+     * Set clubANC
+     *
+     * @param \FDM\ChampBundle\Entity\Club $clubANC
+     *
+     * @return AncienNomClub
+     */
+    public function setClubANC(\FDM\ChampBundle\Entity\Club $clubANC)
+    {
+        $this->clubANC = $clubANC;
+
+        return $this;
+    }
+
+    /**
+     * Get clubANC
+     *
+     * @return \FDM\ChampBundle\Entity\Club
+     */
+    public function getClubANC()
+    {
+        return $this->clubANC;
+    }
+}
