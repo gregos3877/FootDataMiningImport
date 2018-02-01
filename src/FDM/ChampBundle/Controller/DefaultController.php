a07ed7218d1fa5328363d539d3d6fa2323dd70f3
gregos <troch.gregory@gmail.com>
2018-02-01 15:05:46 +0100

    Enregistrement des club par saison de championnat CLEAR

--- src/FDM/ChampBundle/Controller/DefaultController.php
+++ src/FDM/ChampBundle/Controller/DefaultController.php
@@ -7,31 +7,133 @@ use FDM\ChampBundle\Entity\Championnat;
 use FDM\ChampBundle\Entity\Saison;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\EntityManager;
+use FDM\ChampBundle\Entity\Equipe;
+use FDM\ChampBundle\Entity\Club;
 
 class DefaultController extends Controller {
 
 	public function indexAction() {
+		/* Recuperation de l'entityManager */
 		$em = $this->getDoctrine ()->getManager ();
 		
+		/* Selection du championnat de france de ligue 1 et Affichage */
 		$championnat = $em->getRepository ( 'FDMChampBundle:Championnat' )->find ( 1 );
-		echo $championnat->getNomChampionnat () . "<br>";
+		echo $championnat->getNomChampionnat () . "<br><br>";
 		
-		/* Recuperation des equipes pour une saison */
-		$pageHtml = $this->telechargementCurl ( "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_1933-1934" );
 		
-		$this->parserClub ( $pageHtml );
-		// $file = file_get_contents("http://www.lfp.fr/ligue1/classement?saison=100#sai=2&journee1=1&journee2=26&cat=Gen");
 		
-		// $find = htmlentities('<td class="club">');
-		// echo "find : ".$find."<br>";
-		// $debut = strpos(htmlentities($file), $find);
-		// // echo htmlentities($file);
 		
-		// echo "parse".$debut." -<br>";
-		// $html = $this->telechargementCurl("http://www.lfp.fr/ligue1/classement?saison=100#sai=2&journee1=1&journee2=26&cat=Gen");
-		// $this->parserClub($html);
+		/* Recuperation du tableau du classement du championnat sur la page */
+// 		$adresse = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_1933-1934" ;
+
+		
+		$test = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_" ;
+		for ($a=1933; $a<1940;$a++){
+			$date = $a."-".($a+1);
+			$adresseParDate = $test.$date;
+			echo $adresseParDate."<br>";
+			
+			$pageHtml = $this->telechargement ( $adresseParDate);
+			echo "adresse : ".$adresseParDate."<br>";
+			
+			
+			/*
+			 * Parsage de la page telechargé
+			 * Recuperation des equipes qui ont participé a la saison
+			 */
+			$equipes = $this->parseEquipe ( $pageHtml );
+			
+			echo "Nombre d'equipe : " . count ( $equipes ) . "<br><br>";
+			
+			foreach ( $equipes as $eq ) {
+				foreach ( $eq as $i => $var ) {
+					
+					echo $i . "=>" . $var . "<br>";
+					
+					$club = $em->getRepository ( 'FDMChampBundle:Club' )->findOneBy ( array (
+							'nomClub' => $i
+					) );
+					
+					if ($club == null) {
+						echo " aucun club enregistré pour cette equipe  <br>";
+						$club = new Club ();
+						$club->setNomClub ( $i );
+						
+						$creationClub = $this->rechercheCreationClub ( $i );
+						$club->setCreationClub ( $creationClub );
+						$em->persist ( $club );
+					}
+					echo "----------------------------<br>";
+				}
+			}
+		}
+		
+		
+		
+// 		$em->flush ();
 		
 		return $this->render ( 'FDMChampBundle:Default:index.html.twig' );
+	}
+
+	public function rechercheCreationClub($nomClub) {
+		echo "REcherche pour " . $nomClub . "<br>";
+		
+		$adresse = "https://fr.wikipedia.org/wiki/" . $nomClub;
+		$adresse = str_replace ( " ", "_", $adresse );
+		$res = file_get_contents ( $adresse );
+		
+		$d = strpos ( $res, ">Fondation<" );
+		$dCrea = strpos ( $res, "title=", $d );
+		$debut = strpos ( $res, ">", $dCrea );
+		// $f = strpos($res, "");
+		// echo htmlentities(substr($res, $debut+1, 4));
+		return htmlentities ( substr ( $res, $debut + 1, 4 ) );
+		// echo substr($res, $d, 500);
+	}
+
+	public function parseEquipe($pageEquipe) {
+		echo "<br><br> parsage :<br>";
+		
+		$cpt = 0;
+		$cond = true;
+		
+		$debut = 0;
+		$listeEquipe = [ ];
+		
+		while ( $cond ) {
+			
+			$posDebutTR = strpos ( $pageEquipe, "<tr", $debut );
+			$posFinTR = strpos ( $pageEquipe, "</tr", $posDebutTR );
+			
+			$debut = $posFinTR;
+			
+			if ($posDebutTR == 0) {
+				$cond = false;
+			}
+			elseif ($cpt > 0) {
+				// echo "Pos => ".$posDebutTR." / ". $posFinTR;
+				
+				$debutNom = strpos ( $pageEquipe, "title=", $posDebutTR );
+				$finNom = strpos ( $pageEquipe, ">", $debutNom );
+				// echo " | Nom".$debutNom." / ". $finNom;
+				$finPseudo = strpos ( $pageEquipe, "<", $finNom );
+				// echo "NOM equipe : " . substr ( $pageEquipe, $debutNom + 7, ($finNom - $debutNom - 8) ) . " => " . substr ( $pageEquipe, $finNom + 1, ($finPseudo - $finNom - 1) ) . "<br>";
+				
+				$listeEquipe [] = [ 
+						substr ( $pageEquipe, $debutNom + 7, ($finNom - $debutNom - 8) ) => substr ( $pageEquipe, $finNom + 1, ($finPseudo - $finNom - 1) ) 
+				];
+				
+				// echo substr($pageEquipe, $posDebutTR, ($posFinTR - $posDebutTR));
+				
+				// echo $finNom . " / " .$finPseudo ." <br>";
+				
+				// echo ;
+			}
+			
+			$cpt ++;
+		}
+		
+		return $listeEquipe;
 	}
 
 	public function enregistrementEquipeParSaison() {
@@ -49,63 +151,19 @@ class DefaultController extends Controller {
 		$em->flush ();
 	}
 
-	private function telechargementCurl($adresseHtml) {
-// 		$ch = curl_init ( $adresseHtml );
-// 		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
-// 		$res = curl_exec ( $ch );
-// 		curl_close ( $ch );
-// 		echo "Curl ok<br>";
-// 		echo htmlentities ( $res );
+	private function telechargement($adresseHtml) {
+		$res = file_get_contents ( $adresseHtml );
 		
-		$res = file_get_contents($adresseHtml);
-// 		echo htmlentities ( $res );
-		return htmlentities ( $res );
+		$deb = strpos ( $res, "<caption>Classement</caption>" );
+		$deb = $deb + strlen ( "<caption>Classement</caption>" );
+		$fin = strpos ( $res, "</table>", $deb );
+		// echo "pos : " . $deb . " / " . $fin . "<br>";
+		
+		return substr ( $res, $deb, $fin - $deb );
+		
+		// echo htmlentities ( $res );
+		// return htmlentities ( $res );
 		
 		// echo $res;
-	}
-
-	public function parserClub($html) {
-// 		<tr bgcolor="#DCE5E5">
-// 		<td><b><span class="nowrap">2</span></b></td>
-// 		<td align="left"><span class="nowrap"><a href="/wiki/Sporting_Club_fivois" title="Sporting Club fivois">SC Fives</a></span></td>
-// 		<td><b>33</b></td>
-// 		<td>26</td>
-// 		<td style="border-right-style:hidden">13</td>
-// 		<td style="border-right-style:hidden">7</td>
-// 		<td>6</td>
-// 		<td style="border-right-style:hidden">57</td>
-// 		<td style="border-right-style:hidden">31</td>
-// 		<td>1,839</td>
-// 		</tr>
-
-		
-		
-// 		$next = true;
-		$debut = 70572;
-// 		$cpt = 0 ;
-// 		while ( $next == true ) {
-// 			$cpt++;
-			$findMe = htmlentities ( '<tr bgcolor="' );
-			
-			$debut = strpos ( $html, $findMe, $debut );
-			$debutTag = strpos($html, htmlentities ('title="'), $debut);
-// 			$debut = $debut + 1;
-			$fin = strpos ( $html, htmlentities ( '">' ), $debutTag );
-			
-// 			echo "pos => " . $debut . " | " . $fin . "<br>";
-// 			echo "Teg => ".$debutTag."<br>";
-			$htmlResearch = substr ( $html, $debutTag+12, ($fin - $debutTag-12) );
-			
-// 			if (strlen ( $htmlResearch ) > 100) {
-// 				echo "fin<br>";
-// 				$next = false;
-// 			}
-// 			else {
-				echo $htmlResearch . "<br>";
-// 				$debut = $fin ;
-// 			}
-// 		}
-// 		echo "cpt => ".$cpt."<br>";
-		echo "parse ok <br>";
 	}
 }
